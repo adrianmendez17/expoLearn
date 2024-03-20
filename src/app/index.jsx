97609c8aa4c0d082f85dd547076274aac6590b22
adrianmendez17 <adrianmendez17sch@gmail.com>
2024-03-20 15:16:17 -0700

    query graphql api using react query

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList} from 'react-native';
-import exercises from '../../assets/data/exercises.json';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native';
 import ExerciseListItem from "../../src/components/ExerciseListItem";
+import {useQuery} from '@tanstack/react-query';
+import { gql } from 'graphql-request';
+import client from "../graphqlClient";
 
+const exercisesQuery = gql`
+    query exercises($muscle: String, $name: String){
+        exercises(muscle: $muscle, name: $name){
+            name
+            muscle
+            equipment
+        }
+    }
+`
+export default function ExercisesScreen() {
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['exercises'],
+        queryFn: () => client.request(exercisesQuery),
+    });
 
-export default function App() {
+    if (isLoading) {
+        return <ActivityIndicator/>
+    }
+
+    if (error) {
+        return <Text>Failed to fetch exercises.</Text>
+    }
+
     return (
         <View style={styles.container}>
             <FlatList
-                data={exercises}
+                data={data?.exercises}
                 contentContainerStyle={{ gap: 5 }}
                 keyExtractor={(item, index) => item.name + index}
                 renderItem={({ item }) => <ExerciseListItem item={item}/>}
